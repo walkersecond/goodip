ae36a59c2dcf4e687abfd29e282b4c56f4cd6539
Neo Liu <45614573+walkerfirst@users.noreply.github.com>
2025-09-26 15:30:18 +0800

    Update collect_ips.py
    从第6个开始只取16个ip

--- collect_ips.py
+++ collect_ips.py
@@ -4,41 +4,40 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://api.uouin.com/cloudflare.html', 
-        'https://ip.164746.xyz'
-        ]
+urls = [
+    'https://api.uouin.com/cloudflare.html',
+    'https://ip.164746.xyz'
+]
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
-# 检查ip.txt文件是否存在,如果存在则删除它
+# 检查 ip.txt 文件是否存在,如果存在则删除它
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-# 创建一个文件来存储IP地址
+count = 0       # 全局计数器，记录第几个IP
+written = 0     # 已写入数量
+
 with open('ip.txt', 'w') as file:
     for url in urls:
-        # 发送HTTP请求获取网页内容
         response = requests.get(url)
-        
-        # 使用BeautifulSoup解析HTML
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://api.uouin.com/cloudflare.html':
-            elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
+
+        if url in ['https://api.uouin.com/cloudflare.html', 'https://ip.164746.xyz']:
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
-        
-        # 遍历所有元素,查找IP地址
+
         for element in elements:
             element_text = element.get_text()
             ip_matches = re.findall(ip_pattern, element_text)
-            
-            # 如果找到IP地址,则写入文件
-            for ip in ip_matches:
-                file.write(ip + '\n')
 
-print('IP地址已保存到ip.txt文件中。')
+            for ip in ip_matches:
+                count += 1
+                # 从第6个开始，只取偶数，最多16个
+                if count >= 6 and (count % 2 == 0) and written < 16:
+                    file.write(ip + '\n')
+                    written += 1
+
+print('IP地址已保存到 ip.txt 文件中。')
